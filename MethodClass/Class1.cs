b4de84416424585dd6fb39b2457541ff245d7137
Tarran Wylie <124862204+TarranW09@users.noreply.github.com>
2023-07-20 19:51:30 -0600

    Add files via upload

---
+++ MethodClass/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class MathOperations
+{
+    // Void method with two integer parameters
+    public void PerformMathAndDisplay(int num1, int num2)
+    {
+        // Example: Add 5 to the first number and display the second number to the screen
+        int result = num1 + 5;
+        Console.WriteLine($"After adding 5 to the first number ({num1}), the second number ({num2}) is displayed: {num2}");
+    }
+}
+
---
+++ MethodClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Instantiate the MathOperations class
+        MathOperations mathOps = new MathOperations();
+
+        // Call the method with two numbers
+        mathOps.PerformMathAndDisplay(10, 20);
+
+        // Call the method with named parameters
+        mathOps.PerformMathAndDisplay(num1: 7, num2: 15);
+
+        // Infinite loop to keep the console window open
+        while (true)
+        {
+            // Perform any desired actions within the loop
+            // To exit the loop and close the console window, use a break statement or another condition
+        }
+    }
+}
