9cc733ae36fba5bcf388f52828ad2bd161d7e5e8
Tarran Wylie <124862204+TarranW09@users.noreply.github.com>
2023-07-21 18:45:59 -0600

    Add files via upload

---
+++ Operators/Employee.cs
@@ -0,0 +1,46 @@
+﻿// Create the Employee class with properties Id, FirstName, and LastName
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Overload the "==" operator to compare Employee objects by their Id property
+    public static bool operator ==(Employee employee1, Employee employee2)
+    {
+        if (ReferenceEquals(employee1, employee2))
+        {
+            return true; // If both references point to the same object, they are considered equal.
+        }
+
+        if (ReferenceEquals(employee1, null) || ReferenceEquals(employee2, null))
+        {
+            return false; // If one of the objects is null, they are not considered equal.
+        }
+
+        return employee1.Id == employee2.Id; // Compare the Id properties for equality.
+    }
+
+    // Overload the "!=" operator using the "==" operator to maintain consistency
+    public static bool operator !=(Employee employee1, Employee employee2)
+    {
+        return !(employee1 == employee2); // Use the negation of the "=="" operator.
+    }
+
+    // Override the Equals() method for better comparison behavior
+    public override bool Equals(object obj)
+    {
+        if (obj is Employee otherEmployee)
+        {
+            return this == otherEmployee; // Use the overloaded "==" operator for comparison.
+        }
+
+        return false;
+    }
+
+    // Override the GetHashCode() method to satisfy compiler warnings.
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Instantiate two Employee objects and assign values to their properties
+        Employee employee1 = new Employee { Id = 101, FirstName = "John", LastName = "Doe" };
+        Employee employee2 = new Employee { Id = 102, FirstName = "Jane", LastName = "Smith" };
+        Employee employee3 = new Employee { Id = 101, FirstName = "John", LastName = "Doe" }; // An employee with the same Id as employee1
+
+        // Compare the Employee objects using the overloaded "==" operator and display the results
+        Console.WriteLine($"Employee1 == Employee2: {employee1 == employee2}"); // Should be false
+        Console.WriteLine($"Employee1 == Employee3: {employee1 == employee3}"); // Should be true
+
+        // Compare the Employee objects using the overloaded "!=" operator and display the results
+        Console.WriteLine($"Employee1 != Employee2: {employee1 != employee2}"); // Should be true
+        Console.WriteLine($"Employee1 != Employee3: {employee1 != employee3}"); // Should be false
+
+        // Infinite loop to keep the console window open
+        while (true)
+        {
+            // Perform any desired actions within the loop
+            // To exit the loop and close the console window, use a break statement or another condition
+        }
+    }
+}
